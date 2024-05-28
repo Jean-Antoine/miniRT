9623ff853c8c58a72ee520fc9f8390ca7d814de2
Jean-antoine Da silva <jeada-si@bess-f3r1s5.clusters.42paris.fr>
2024-05-28 14:03:41 +0200

    check minimum scene

--- srcs/parsing/ft_parse.c
+++ srcs/parsing/ft_parse.c
@@ -6,19 +6,37 @@
 /*   By: jeada-si <jeada-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 13:35:36 by lpaquatt          #+#    #+#             */
-/*   Updated: 2024/05/28 12:00:08 by jeada-si         ###   ########.fr       */
+/*   Updated: 2024/05/28 14:02:33 by jeada-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini_rt.h"
 
-void	ft_error_line(int i, char *str)
+static void	ft_error_line(int i, char *str)
 {
 	ft_putstr_fd("line ", STDERR_FILENO);
 	ft_putnbr_fd(i, STDERR_FILENO);
 	ft_putstr_fd(": ", STDERR_FILENO);
 	ft_putstr_fd(str, STDERR_FILENO);
 	ft_putstr_fd("\n", STDERR_FILENO);
+}
+
+static int	ft_min_scene(char *line)
+{
+	static int	camera = 0;
+	static int	ambiant = 0;
+
+	if (!line)
+	{
+		if (camera && ambiant)
+			return (EXIT_SUCCESS);
+		return (ft_error("there must be at least one camera "
+				"and one ambiant light", FALSE));
+	}
+	camera += line[0] == 'C';
+	ambiant += line[0] == 'A';
+	free(line);
+	return (EXIT_FAILURE);
 }
 
 static int	ft_parse_scene(int fd, t_scene *scene)
@@ -39,15 +57,16 @@ static int	ft_parse_scene(int fd, t_scene *scene)
 		exit_code = ft_parse_line(line, scene);
 		if (exit_code)
 			ft_error_line(i, line);
-		free(line);
+		ft_min_scene(line);
 		if (exit_code)
 			break ;
 	}
 	get_next_line(fd, TRUE);
 	close(fd);
-	return (exit_code);
+	if (exit_code)
+		return (EXIT_FAILURE);
+	return (ft_min_scene(NULL));
 }
-
 
 int	ft_parse(t_scene *scene, char *file)
 {
@@ -59,7 +78,5 @@ int	ft_parse(t_scene *scene, char *file)
 		return (EXIT_FAILURE);
 	if (ft_parse_scene(fd, scene))
 		return (EXIT_FAILURE);
-	// if (ft_check_scene(scene))
-	// 	return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
--- srcs/parsing/ft_parse_camera.c
+++ srcs/parsing/ft_parse_camera.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_parse_camera.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lpaquatt <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jeada-si <jeada-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 11:26:11 by lpaquatt          #+#    #+#             */
-/*   Updated: 2024/05/28 12:33:15 by lpaquatt         ###   ########.fr       */
+/*   Updated: 2024/05/28 13:39:53 by jeada-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,15 @@ int	ft_parse_camera(char **args, t_camera *dest)
 	if (ft_strcmp(args[0], "C"))
 		return (2);
 	if (++nb_cameras > 1)
-		return (ft_error("Invalid data\n", FALSE));
+		return (ft_error("more than one camera", FALSE));
 	if (ft_tabsize(args) != 4)
-		return (ft_error("Invalid data\n", FALSE));
+		return (ft_error("wrong number of parameters", FALSE));
 	exit_code = (ft_set_point(&dest->position, args[1])
-			+ ft_set_vector(&dest->direction, args[2])
-			+ ft_set_scalar(&dest->fov, args[3]));
+			|| ft_set_vector(&dest->direction, args[2])
+			|| ft_set_scalar(&dest->fov, args[3]));
 	if (exit_code == EXIT_SUCCESS
 		&& (dest->fov < 0
 			|| dest->fov > 180))
-		return (ft_error("Invalid data\n", FALSE));
-	return (exit_code != 0);
+		return (ft_error("FOV must be between 0 and 180", FALSE));
+	return (exit_code);
 }
