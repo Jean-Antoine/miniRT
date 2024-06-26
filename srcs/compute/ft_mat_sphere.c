a1027115944064c7598776e2b06905a4f721b6b7
leontinepaq <lpaquatt@made-f0Ar2s8.clusters.42paris.fr>
2024-06-26 13:21:19 +0200

    good mat sphere

--- srcs/compute/ft_mat_sphere.c
+++ srcs/compute/ft_mat_sphere.c
@@ -6,7 +6,7 @@
 /*   By: lpaquatt <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 12:24:08 by jeada-si          #+#    #+#             */
-/*   Updated: 2024/06/25 16:47:55 by lpaquatt         ###   ########.fr       */
+/*   Updated: 2024/06/26 13:17:24 by lpaquatt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ void	ft_mat_sphere(t_object *sphere)
 {
 	sphere->transform = ft_mat_prod(
 			sphere->transform,
-			ft_scaling(sphere->diameter / 2.0, sphere->diameter / 2.0,
-				sphere->diameter / 2.0));
-	sphere->transform = ft_mat_prod(
-			sphere->transform,
 			ft_translation(
 				sphere->position.x, sphere->position.y, sphere->position.z));
+	sphere->transform = ft_mat_prod(
+			sphere->transform,
+			ft_scaling(sphere->diameter / 2.0, sphere->diameter / 2.0,
+				sphere->diameter / 2.0));
 	sphere->transform = ft_mat_inv(sphere->transform);
 }
