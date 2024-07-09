8b8cd45f84df1c04ba4430c487bd02e27461fc15
Jean-antoine Da silva <jeada-si@made-f0Ar2s10.clusters.42paris.fr>
2024-07-09 16:11:39 +0200

    calloc normal

--- libft/srcs/base/ft_calloc.c
+++ libft/srcs/base/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: jeada-si <jeada-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 09:10:52 by jeada-si          #+#    #+#             */
-/*   Updated: 2024/07/09 16:00:30 by jeada-si         ###   ########.fr       */
+/*   Updated: 2024/07/09 16:11:26 by jeada-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,7 @@
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void		*out;
-	static int	boundary;
-	static int	x = 0;
 
-	if (x == 0)
-	{
-		srand(time(NULL));
-		boundary = rand() % 1000;
-		errno = 12;
-	}		
-	if (++x > boundary)
-		return (NULL);
 	if (nmemb == 0 || size == 0)
 		return ((void *)malloc(0));
 	if (nmemb > ((size_t)(-1)) / size)
